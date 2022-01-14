091dde6fed8d0d9f0f43caca5f2bfe2cb8280000
Dogo-Lab <97763126+LabScripts@users.noreply.github.com>
2022-01-15 00:28:58 +0200

    Add files via upload

--- shared/main.lua
+++ shared/main.lua
@@ -6,14 +6,14 @@ Config.BlipSize = 0.8 -- Size of the blips.
 
 -- Configure anything except: spawnedPlants, DrugPlantsA and DrugCoords. These shall never be touched!
 Config.Fields = {
-    A = {FieldCoords = vector3(-2146.8, 2686.72, 2.92), label = 'Heroin', itemName = 'heroin', amount = 5, jobRestricted = true, jobs = {'ballas','unicorn','police'}, neededCops = 1, DrugProp = 'prop_cs_plant_01', duration = 1000, animDict= 'random@domestic', anim = 'pickup_low', blip = true, blipSprite = 403, blipColour = 0, spawnedPlants = 0, DrugPlantsA = {}, DrugCoords},
-	B = {FieldCoords = vector3(-2599.36, 2833.32, 2.92), label = 'Crack', itemName = 'crack', amount = 5, jobRestricted = false, jobs = {''}, neededCops = 0, DrugProp = 'ex_office_swag_drugbag2', duration = 1000, animDict= 'random@domestic', anim = 'pickup_low', blip = true, blipSprite = 497, blipColour = 1, spawnedPlants = 0, DrugPlantsA = {}, DrugCoords},
+    A = {FieldCoords = vector3(3522.56, 2569.64, 7.76), label = 'Heroin', itemName = 'heroin', amount = 5, jobRestricted = true, jobs = {'ballas','unicorn','police'}, neededCops = 1, DrugProp = 'prop_cs_plant_01', duration = 1000, animDict= 'random@domestic', anim = 'pickup_low', blip = true, blipSprite = 403, blipColour = 0, spawnedPlants = 0, DrugPlantsA = {}, DrugCoords},
+	B = {FieldCoords = vector3(-1810.4, 2657.96, 3.4), label = 'Crack', itemName = 'crack', amount = 5, jobRestricted = false, jobs = {''}, neededCops = 0, DrugProp = 'ex_office_swag_drugbag2', duration = 1000, animDict= 'random@domestic', anim = 'pickup_low', blip = true, blipSprite = 497, blipColour = 1, spawnedPlants = 0, DrugPlantsA = {}, DrugCoords},
 }
 
 -- Configure anything to your liking.
 Config.Labs = {
-    A = {LabCoords = vector3(-2080.04, 2608.92, 3.08), neededLabel = 'Heroin', givenLabel = 'Crack', neededItem = 'heroin', neededAmount = 5, givenItem = 'crack', givenAmount = 1, jobRestricted = true, jobs = {'ballas','unicorn'}, neededCops = 1, duration = 6000, animDict= 'missmechanic', anim = 'work2_base', blip = true, blipSprite = 403, blipColour = 0},
-    B = {LabCoords = vector3(-2082.28, 2614.32, 3.08), neededLabel = 'Crack', givenLabel = 'Heroin', neededItem = 'crack', neededAmount = 5, givenItem = 'heroin', givenAmount = 1, jobRestricted = false, jobs = {}, neededCops = 0, duration = 6000, animDict= 'missmechanic', anim = 'work2_base', blip = true, blipSprite = 497, blipColour = 0},
+    A = {LabCoords = vector3(1391.84, 3605.88, 38.96), neededLabel = 'Heroin', givenLabel = 'Crack', neededItem = 'heroin', neededAmount = 5, givenItem = 'crack', givenAmount = 1, jobRestricted = true, jobs = {'ballas','unicorn'}, neededCops = 1, duration = 6000, animDict= 'missmechanic', anim = 'work2_base', blip = true, blipSprite = 403, blipColour = 0},
+    B = {LabCoords = vector3(2433.88, 4969.2, 42.36), neededLabel = 'Crack', givenLabel = 'Heroin', neededItem = 'crack', neededAmount = 5, givenItem = 'heroin', givenAmount = 1, jobRestricted = false, jobs = {}, neededCops = 0, duration = 6000, animDict= 'missmechanic', anim = 'work2_base', blip = true, blipSprite = 497, blipColour = 0},
 }
  
 
